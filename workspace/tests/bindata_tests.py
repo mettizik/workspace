526e7de5c74ec12417ec490bdf8afb4d854b5905
Mokych Andrey <mokych.apriorit@gmail.com>
2017-06-23 23:10:16 +0300

    implemented first test

--- workspace/tests/bindata_tests.py
+++ workspace/tests/bindata_tests.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
+from workspace.bindata.struct_unpack import smart_unpack
 
 class struct_processor(TestCase):
-    def test_sample(self):
-        pass
+    def test_smart_unpack_can_unpack_integer(self):
+        res = smart_unpack({'struct':[{'type':'n:1', 'name':'1'}], 'data':bytes([1])})
+        self.assertEqual(res['name'], '1')
+        self.assertEqual(res['value'], 1)
+        self.assertEqual(res['raw'], bytes([1]))
 
