39450df1ea36bf67646e863c441ab463a5fa93db
Mokych Andrey <mokych.apriorit@gmail.com>
2017-06-23 23:10:55 +0300

    fixed init script

--- workspace/bindata/__init__.py
+++ workspace/bindata/__init__.py
@@ -1,9 +1,3 @@
 """
 Python package for common utilities to work with binary data
 """
-from os.path import abspath, dirname
-from sys import path
-
-path.append(abspath(dirname(__file__)))
-
-import tests
