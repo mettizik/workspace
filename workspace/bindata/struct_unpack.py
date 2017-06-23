a439841bc58609f30f3129f0d718af10909bbebe
Mokych Andrey <mokych.apriorit@gmail.com>
2017-06-23 23:37:39 +0300

    Refactored test and module

--- workspace/bindata/struct_unpack.py
+++ workspace/bindata/struct_unpack.py
@@ -1,2 +1,37 @@
-def smart_unpack(struct_def, bindata, **kwargs):
-    pass
+import io
+from sys import byteorder as SystemByteorder
+class Struct:
+    """
+class that represents a binary structure
+    """
+
+    class Field:
+        """
+    class that represents a binary structure
+        """
+
+        TYPE_NUMBER = 0
+        TYPE_STRING = 1
+        TYPE_BYTES  = 2
+
+        def __init__(self, field_name: str, field_type: int, size=None):
+            """
+        create a field of a structure with given name, type and size
+            """
+            self.name = field_name
+            self.type = field_type
+            self.size = size
+            self.value = None
+            self.raw_data = None
+
+    def __init__(self, name:str, fields:list):
+        """
+    Create a structure, with provided names and fields
+        """
+        self._name = name
+        self._fields = fields
+
+def unpack_field(field: Struct.Field, bindata:io.RawIOBase):
+    raw_data = bindata.read(field.size)
+    field.value = int.from_bytes(raw_data, byteorder=SystemByteorder)
+    field.raw_data = raw_data
--- workspace/tests/bindata_tests.py
+++ workspace/tests/bindata_tests.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
-from workspace.bindata.struct_unpack import smart_unpack
+from workspace.bindata.struct_unpack import *
 
 class struct_processor(TestCase):
     def test_smart_unpack_can_unpack_integer(self):
-        res = smart_unpack({'struct': [{'type': 'n:1', 'name': '1'}]}, bytes([1]))
-        self.assertEqual(res['name'], '1')
-        self.assertEqual(res['value'], 1)
-        self.assertEqual(res['raw'], bytes([1]))
+        field = Struct.Field('1', Struct.Field.TYPE_NUMBER, size=1)
+        data = io.BytesIO(bytes([1]))
+        unpack_field(field, data)
+        self.assertEqual(field.value, 1)
+        self.assertEqual(field.raw_data, data.getvalue())
 
